308fa991cd332f36346e6550346c7767343deede
zaizai <15437941845zaizai>
2019-06-08 00:33:11 +0800

    登录

--- ssc-h5/js/public.js
+++ ssc-h5/js/public.js
@@ -3,3 +3,27 @@ var BASE_URL = "http://192.168.31.174:8080/hwgame/";
 mui.plusReady(function() {
 	plus.navigator.setStatusBarBackground("#000000");
 });
+
+//判断当前是否登录
+function getUserInfo(){
+	var userinfo = sessionStorage.getItem("userInfo");
+	if(userinfo){
+		return userinfo;
+	}else{
+		var aniShow = mui.os.plus ? "slide-in-right" : "zoom-fade-out";
+		mui.openWindow({
+			url: "../other/login.html",
+			id: "info",
+			styles: {
+				popGesture: "close",
+				statusbar: {
+					background: "#f7f7f7"
+				}
+			},
+			show: {
+				aniShow: aniShow,
+				duration: 300
+			}
+		});
+	}
+}
