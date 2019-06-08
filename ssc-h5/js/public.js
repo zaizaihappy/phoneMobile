69ac7530a5a70705d62f0310225c32c9aa3e016c
zaizai <15437941845zaizai>
2019-06-08 13:17:30 +0800

    qwe

--- ssc-h5/js/public.js
+++ ssc-h5/js/public.js
@@ -3,14 +3,15 @@ var BASE_URL = "http://192.168.31.174:8080/hwgame/";
 mui.plusReady(function() {
 	plus.navigator.setStatusBarBackground("#000000");
 });
-
+//禁止界面晃动
+document.getElementsByClassName("mui-content")[0].ontouchmove = function (e) {e.preventDefault();};
+var aniShow = mui.os.plus ? "slide-in-right" : "zoom-fade-out";
 //判断当前是否登录
 function getUserInfo(){
 	var userinfo = sessionStorage.getItem("userInfo");
 	if(userinfo){
 		return userinfo;
 	}else{
-		var aniShow = mui.os.plus ? "slide-in-right" : "zoom-fade-out";
 		mui.toast("请登录后查看")
 		setTimeout(function(){
 			mui.openWindow({
@@ -29,4 +30,29 @@ function getUserInfo(){
 			});
 		},1500)
 	}
+}
+
+//公用底部导航栏跳转
+mui('.mui-bar-tab').on('tap', 'a', function(e) {
+	var targetTab = this.getAttribute('href');
+	mui.openWindow({
+		url: targetTab,
+		extras: {
+			id: '100'
+		},
+		styles: {
+			popGesture: "close",
+			statusbar: {
+				background: "#f7f7f7"
+			}
+		},
+		show: {
+			aniShow: aniShow,
+			duration: 300
+		}
+	});
+});
+
+function btnClick(){
+	window.history.back(-1)
 }
---
+++ ssc-h5/js/userCenter/main.js
@@ -0,0 +1,23 @@
+mui.init();
+// getUserInfo();
+//禁止界面元素晃动
+document.getElementsByClassName("mui-content")[0].ontouchmove = function (e) {e.preventDefault();};	
+mui('.mui-grid-9').on('tap', '.mui-col-sm-3', function(e) {
+	var gourl = this.getAttribute('data-gourl');//获取跳转的界面路径
+	mui.openWindow({
+		url: gourl,
+		extras: {
+			id: '100'
+		},
+		styles: {
+			popGesture: "close",
+			statusbar: {
+				background: "#f7f7f7"
+			}
+		},
+		show: {
+			aniShow: aniShow,
+			duration: 300
+		}
+	});
+});
